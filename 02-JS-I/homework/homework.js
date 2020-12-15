7d13b8f68cf29389dde15462961d04eeba6bc4e0
Kowal64 <kowalczukagustin@gmail.com>
2020-12-15 00:52:52 -0300

    modifica homework 02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -1,7 +1,7 @@
 // En estas primeras 6 preguntas, reemplaza `null` por la respuesta
 
 // Crea una variable "string", puede contener lo que quieras:
-const nuevaString = 'Hola Mundo';
+const nuevaString = 'Hola';
 
 // Crea una variable numérica, puede ser cualquier número:
 const nuevoNum = 64;
@@ -35,29 +35,28 @@ function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
   // Tu código:
-  var a=x+y
-  return(a)
+  
+  return(x+y)
 }
 
 function resta(x, y) {
   // Resta "y" de "x" y devuelve el valor
   // Tu código:
-  var a=x-y
-  return(a)
+  
+  return(x-y)
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
   // Tu código:
-  var a=x*y
-  return(a)
+  return(x*y)
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
   // Tu código:
-  var a=x/y
-  return(a)
+  
+  return(x/y)
 }
 
 function sonIguales(x, y) {
